f56212527e2d3690fb84ff63a021a8cb998da51d
Max IV <maxhard001@gmail.com>
2022-08-24 15:13:52 +0300

    random words util - commented

--- com/gmail/maxhard001/util/RandomWords.java
+++ com/gmail/maxhard001/util/RandomWords.java
@@ -1,5 +1,10 @@
 package com.gmail.maxhard001.util;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 public class RandomWords {
 
     public static String getWord(int numberOf) {
@@ -15,8 +20,26 @@ public class RandomWords {
         return getWord(wordRandom);
     }
 
+    public static long getTimeMsOfInsert(List<Object> list) {
+            //напишите тут ваш код
+        Date currentTime = new Date();
+        insert1000000(list);
+        Date newTime = new Date();
+        long msDelay = newTime.getTime() - currentTime.getTime(); //вычисляем разницу
+        System.out.println("Результат в миллисекундах: " + msDelay);
+        return msDelay;
+     
+    }
 
+    public static void insert1000000(List<Object> list) {
+        for (int i = 0; i < 1000000; i++) {
+            list.add(0, new Object());
+        }
+    }
+    
     public static void main(String[] args) {
         System.out.println(wordRandomSize(4, 9));
+        getTimeMsOfInsert(new ArrayList<Object>());
+        getTimeMsOfInsert(new LinkedList<Object>());
     }
 }
