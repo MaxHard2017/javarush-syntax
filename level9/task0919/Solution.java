87283206199169d4a4fa76a899a49538b7f43940
Max IV <maxhard001@gmail.com>
2022-08-25 23:30:14 +0300

    try-catch

---
+++ level9/task0919/Solution.java
@@ -0,0 +1,16 @@
+package level9.task0919;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            divideByZero();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void  divideByZero() {
+        System.out.println(100/0);
+    }
+}
