2bf4530b8096a88797a790259cb8c19f75019f64
Max IV <maxhard001@gmail.com>
2022-09-05 21:24:32 +0300

    ХЗ о чем... конструкторы

---
+++ level10/task1013/Solution.java
@@ -0,0 +1,106 @@
+package level10.task1013;
+
+
+
+/*
+ *  Напиши класс Human с 6 полями.
+    Придумай и реализуй 10 различных конструкторов для него.
+    Требования:
+        •    Программа не должна считывать данные с клавиатуры.
+        •    В классе Human должно быть 6 полей.
+        •    Все поля класса Human должны быть private.
+        •    В классе Human должно быть 10 конструкторов.
+        •    Все конструкторы класса Human должны быть public.
+ */
+public class Solution {
+    public static class Human {
+        private String name;
+        private String secondName;
+        private String middleName;
+        private String address;
+        private int age;
+        private boolean sex;
+        public Human(String name, String secondName, String middleName, String address, int age, boolean sex) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = middleName;
+            this.address = address;
+            this.age = age;
+            this.sex = sex;
+        }
+        public Human(String name, String secondName, String middleName, String address, int age) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = middleName;
+            this.address = address;
+            this.age = age;
+            this.sex = true;
+        }
+        public Human(String name, String secondName, String middleName, String address, boolean sex) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = middleName;
+            this.address = address;
+            this.age = 25;
+            this.sex = sex;
+        }
+        public Human(String name, String secondName, String middleName, String address) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = middleName;
+            this.address = address;
+            this.age = 25;
+            this.sex = true;
+        }
+        
+        public Human(String name, String secondName, String address, int age, boolean sex) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = "";
+            this.address = address;
+            this.age = age;
+            this.sex = sex;
+        }
+        public Human(String name, String secondName, int age, boolean sex) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = "";
+            this.address = "homeless";
+            this.age = age;
+            this.sex = sex;
+        }
+        public Human(String name, String secondName, String address, boolean sex) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = "";
+            this.address = address;
+            this.age = 25;
+            this.sex = sex;
+        }
+        public Human(String name, String secondName, String address, int age) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = "";
+            this.address = address;
+            this.age = age;
+            this.sex = true;
+        }
+        public Human(String name, String secondName) {
+            this.name = name;
+            this.secondName = secondName;
+            this.middleName = "";
+            this.address = "homeless";
+            this.age = 25;
+            this.sex = true;
+        }
+        public Human() {
+            this.name = "name";
+            this.secondName = "secondName";
+            this.middleName = "middleName";
+            this.address = "address";
+            this.age = 25;
+            this.sex = true;
+        }
+             
+    }
+}
